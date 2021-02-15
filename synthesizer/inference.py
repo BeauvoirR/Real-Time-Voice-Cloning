6adb3cee0b189a206a0938ca380bc771d84c5273
Renee Beauvoir <renee@teammobot.com>
2021-02-14 20:29:21 -0500

    PEP-8 auto-formatting

--- synthesizer/inference.py
+++ synthesizer/inference.py
@@ -1,7 +1,7 @@
 from synthesizer.tacotron2 import Tacotron2
 from synthesizer.hparams import hparams
 from multiprocess.pool import Pool  # You're free to use either one
-#from multiprocessing import Pool   # 
+#from multiprocessing import Pool   #
 from multiprocess.context import SpawnContext
 from synthesizer import audio
 from pathlib import Path
@@ -15,17 +15,17 @@ import librosa
 class Synthesizer:
     sample_rate = hparams.sample_rate
     hparams = hparams
-    
+
     def __init__(self, checkpoints_dir: Path, verbose=True, low_mem=False, seed=None):
         """
         Creates a synthesizer ready for inference. The actual model isn't loaded in memory until
         needed or until load() is called.
-        
+
         :param checkpoints_dir: path to the directory containing the checkpoint file as well as the
         weight files (.data, .index and .meta files)
         :param verbose: if False, only tensorflow's output will be printed TODO: suppress them too
-        :param low_mem: if True, the model will be loaded in a separate process and its resources 
-        will be released after each usage. Adds a large overhead, only recommended if your GPU 
+        :param low_mem: if True, the model will be loaded in a separate process and its resources
+        will be released after each usage. Adds a large overhead, only recommended if your GPU
         memory is low (<= 2gb)
         :param seed: optional integer for seeding random number generators when initializing model
         This makes the synthesizer output consistent for a given embedding and input text.
@@ -63,7 +63,7 @@ class Synthesizer:
         Whether the model is loaded in GPU memory.
         """
         return self._model is not None
-    
+
     def load(self):
         """
         Effectively loads the model to GPU memory given the weights file that was passed in the
@@ -73,7 +73,7 @@ class Synthesizer:
             raise Exception("Cannot load the synthesizer permanently in low mem mode")
         tf.compat.v1.reset_default_graph()
         self._model = Tacotron2(self.checkpoint_fpath, hparams, seed=self._seed)
-            
+
     def synthesize_spectrograms(self, texts: List[str],
                                 embeddings: Union[np.ndarray, List[np.ndarray]],
                                 return_alignments=False):
@@ -81,11 +81,11 @@ class Synthesizer:
         Synthesizes mel spectrograms from texts and speaker embeddings.
 
         :param texts: a list of N text prompts to be synthesized
-        :param embeddings: a numpy array or list of speaker embeddings of shape (N, 256) 
-        :param return_alignments: if True, a matrix representing the alignments between the 
+        :param embeddings: a numpy array or list of speaker embeddings of shape (N, 256)
+        :param return_alignments: if True, a matrix representing the alignments between the
         characters
         and each decoder output step will be returned for each spectrogram
-        :return: a list of N melspectrograms as numpy arrays of shape (80, Mi), where Mi is the 
+        :return: a list of N melspectrograms as numpy arrays of shape (80, Mi), where Mi is the
         sequence length of spectrogram i, and possibly the alignments.
         """
         if not self._low_mem:
@@ -95,12 +95,12 @@ class Synthesizer:
                 self.load()
             specs, alignments = self._model.my_synthesize(embeddings, texts)
         else:
-            # Low memory inference mode: load the model upon every request. The model has to be 
-            # loaded in a separate process to be able to release GPU memory (a simple workaround 
+            # Low memory inference mode: load the model upon every request. The model has to be
+            # loaded in a separate process to be able to release GPU memory (a simple workaround
             # to tensorflow's intricacies)
             specs, alignments = Pool(1, context=SpawnContext()).starmap(Synthesizer._one_shot_synthesize_spectrograms,
                                                 [(self.checkpoint_fpath, embeddings, texts, self._seed)])[0]
-    
+
         return (specs, alignments) if return_alignments else specs
 
     @staticmethod
@@ -109,22 +109,22 @@ class Synthesizer:
         tf.compat.v1.reset_default_graph()
         model = Tacotron2(checkpoint_fpath, hparams, seed=seed)
         specs, alignments = model.my_synthesize(embeddings, texts)
-        
+
         # Detach the outputs (not doing so will cause the process to hang)
         specs, alignments = [spec.copy() for spec in specs], alignments.copy()
-        
+
         # Close cuda for this process
         model.session.close()
         numba.cuda.select_device(0)
         numba.cuda.close()
-        
+
         return specs, alignments
 
     @staticmethod
     def load_preprocess_wav(fpath):
         """
         Loads and preprocesses an audio file under the same conditions the audio files were used to
-        train the synthesizer. 
+        train the synthesizer.
         """
         wav = librosa.load(str(fpath), hparams.sample_rate)[0]
         if hparams.rescale:
@@ -134,17 +134,17 @@ class Synthesizer:
     @staticmethod
     def make_spectrogram(fpath_or_wav: Union[str, Path, np.ndarray]):
         """
-        Creates a mel spectrogram from an audio file in the same manner as the mel spectrograms that 
+        Creates a mel spectrogram from an audio file in the same manner as the mel spectrograms that
         were fed to the synthesizer when training.
         """
         if isinstance(fpath_or_wav, str) or isinstance(fpath_or_wav, Path):
             wav = Synthesizer.load_preprocess_wav(fpath_or_wav)
         else:
             wav = fpath_or_wav
-        
+
         mel_spectrogram = audio.melspectrogram(wav, hparams).astype(np.float32)
         return mel_spectrogram
-    
+
     @staticmethod
     def griffin_lim(mel):
         """
